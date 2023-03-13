9c3ac24c18d099d2e54080c1ba2b343cd174528f
dashe <dashenka.kazanceva@gmail.com>
2023-03-14 03:00:25 +0700

    Добавили игру калькулятор

---
+++ src/games/even.js
@@ -0,0 +1,14 @@
+import baseGame from '../index.js';
+import getRandomInt from '../randomizer.js';
+
+const gameTask = 'Answer "yes" if the number is even, otherwise answer "no"';
+
+const isEvenNum = (num) => (num % 2 === 0);
+
+const theGame = () => {
+  const question = getRandomInt(1, 100);
+  const rightAnswer = isEvenNum(question) ? 'yes' : 'no';
+  return [question, rightAnswer];
+};
+
+export default () => baseGame(gameTask, theGame);
