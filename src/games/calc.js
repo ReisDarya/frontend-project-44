2c4130446bf31d7cccdb6ffe10948abc3d283d0f
dashe <dashenka.kazanceva@gmail.com>
2023-03-24 16:07:35 +0700

    линукс

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,28 +1,28 @@
-import baseGame from '../index.js'
-import getRandomInt, { getRandomOperand } from '../randomizer.js'
+import baseGame from '../index.js';
+import getRandomInt, { getRandomOperand } from '../randomizer.js';
 
-const gameTask = 'What is the result of the expression?'
+const gameTask = 'What is the result of the expression?';
 
 const getRightAnswer = (a, b, c) => {
-  let result
+  let result;
 
   if (c === '+') {
-    result = a + b
+    result = a + b;
   } if (c === '-') {
-    result = a - b
+    result = a - b;
   } if (c === '*') {
-    result = a * b
+    result = a * b;
   }
-  return result
-}
+  return result;
+};
 
 const theGame = () => {
-  const leftOperand = getRandomInt(1, 10)
-  const rightOperand = getRandomInt(1, 10)
-  const operator = getRandomOperand()
-  const question = `${leftOperand} ${operator} ${rightOperand}`
-  const rightAnswer = String(getRightAnswer(leftOperand, rightOperand, operator))
-  return [question, rightAnswer]
-}
+  const leftOperand = getRandomInt(1, 10);
+  const rightOperand = getRandomInt(1, 10);
+  const operator = getRandomOperand();
+  const question = `${leftOperand} ${operator} ${rightOperand}`;
+  const rightAnswer = String(getRightAnswer(leftOperand, rightOperand, operator));
+  return [question, rightAnswer];
+};
 
-export default () => baseGame(gameTask, theGame)
+export default () => baseGame(gameTask, theGame);
