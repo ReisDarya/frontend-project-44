575583d31d40aa2785d828ea21147ffcfd947660
dashe <dashenka.kazanceva@gmail.com>
2023-03-24 15:49:28 +0700

    линтер

--- src/games/even.js
+++ src/games/even.js
@@ -1,14 +1,14 @@
-import baseGame from '../index.js';
-import getRandomInt from '../randomizer.js';
+import baseGame from '../index.js'
+import getRandomInt from '../randomizer.js'
 
-const gameTask = 'Answer "yes" if the number is even, otherwise answer "no"';
+const gameTask = 'Answer "yes" if the number is even, otherwise answer "no"'
 
-const isEvenNum = (num) => (num % 2 === 0);
+const isEvenNum = (num) => (num % 2 === 0)
 
 const theGame = () => {
-  const question = getRandomInt(1, 100);
-  const rightAnswer = isEvenNum(question) ? 'yes' : 'no';
-  return [question, rightAnswer];
-};
+  const question = getRandomInt(1, 100)
+  const rightAnswer = isEvenNum(question) ? 'yes' : 'no'
+  return [question, rightAnswer]
+}
 
-export default () => baseGame(gameTask, theGame);
+export default () => baseGame(gameTask, theGame)
