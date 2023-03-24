90fc11b973eed135bdf8cd03d58e0770567e1e63
dashe <dashenka.kazanceva@gmail.com>
2023-03-24 15:55:25 +0700

    линукс

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
-export const gamesRounds = 3;
+export const gamesRounds = 3
 
 const baseGame = (gameTask, theGame) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(`${gameTask}`);
+  console.log('Welcome to the Brain Games!')
+  const userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log(`${gameTask}`)
 
   for (let i = 0; i < gamesRounds; i += 1) {
-    const [question, rightAnswer] = theGame();
-    console.log(`Question: ${question}`);
-    const userAnswer = readlineSync.question('Your answer: ');
+    const [question, rightAnswer] = theGame()
+    console.log(`Question: ${question}`)
+    const userAnswer = readlineSync.question('Your answer: ')
 
     if (userAnswer !== rightAnswer) {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}`)
+      console.log(`Let's try again, ${userName}!`)
+      return
     }
-    console.log('Correct!');
+    console.log('Correct!')
   }
-  console.log(`Congratulations, ${userName}!`);
-};
+  console.log(`Congratulations, ${userName}!`)
+}
 
-export default baseGame;
+export default baseGame
--- src/randomizer.js
+++ src/randomizer.js
@@ -1,10 +1,10 @@
 export default (min, max) => {
-    const randomInt = Math.random() * (max - min + 1) + min;
-    return Math.floor(randomInt);
-  };
-  
-  export const getRandomOperand = () => {
-    const arr = ['+', '*', '-'];
-    const operand = arr[Math.floor(Math.random() * arr.length)];
-    return operand;
-  };
+  const randomInt = Math.random() * (max - min + 1) + min
+  return Math.floor(randomInt)
+}
+
+export const getRandomOperand = () => {
+  const arr = ['+', '*', '-']
+  const operand = arr[Math.floor(Math.random() * arr.length)]
+  return operand
+}
